623c8b20ab6548df2f17fbaaa6db315eb2867dd5
Ben <ben.leavitt@cybera.ca>
2019-09-03 16:15:15 +0000

    Tuning

--- apps/docker_files/test.sh
+++ apps/docker_files/test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+x=0
 while :
 do
-  if curl http://127.0.0.1/polls/
+  if curl -s http://127.0.0.1/polls/
   then
     echo Up and running
     break
@@ -9,4 +10,10 @@ do
     echo Still waiting
     sleep 10
   fi
+  if x > 60
+    then
+      echo Timeout
+      break
+  fi
+  x=`expr $x + 1`
 done
