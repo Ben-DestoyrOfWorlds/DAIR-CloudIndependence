3e6bd40b5df4cc47eb2e9affea46fa3ab444edd2
Ben <ben.leavitt@cybera.ca>
2019-09-03 16:54:39 +0000

    Making the wait phase timeout eventually

--- apps/docker_files/test.sh
+++ apps/docker_files/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-x=0
+x=1
 while :
 do
   if curl http://127.0.0.1/polls/
@@ -7,13 +7,12 @@ do
     echo Up and running
     break
   else
-    echo Still waiting
+    echo Still waiting loop $x / 60
     sleep 10
   fi
-  if x > 60
-    then
-      echo Timeout
-      break
+  if [ $x -gt 59 ]; then
+    echo Timeout
+    break
   fi
   x=`expr $x + 1`
 done
