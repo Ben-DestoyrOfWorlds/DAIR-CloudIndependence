be1478f20d2326d56f21c2cdbef7a5e98f129715
ben.leavitt@cybera.ca <ben.leavitt@cybera.ca>
2019-09-23 19:50:22 +0000

    Adding jenkins setup script

---
+++ setup-Jenkins.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install docker.io
+sudo docker run -d -v jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --name jenkins -p 80:8080 -p 50000:50000 jenkins/jenkins:lts
+sleep 15
+echo "Initial Admin Password below"
+INITPASS=`sudo cat /var/lib/docker/volumes/jenkins_home/_data/secrets/initialAdminPassword`
+echo $INITPASS
+while :
+do
+	TESTPASS=`sudo cat /var/lib/docker/volumes/jenkins_home/_data/secrets/initialAdminPassword`
+	if [ "$TESTPASS" != "$INITPASS" ]; then
+		echo $TESTPASS
+		break
+	fi
+done
+sleep 10
+sudo docker restart jenkins
